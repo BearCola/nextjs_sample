7bfa16c1c6bf21b21b0eae63a2e7c103a3dbc61a
Sangin <sangin.kang@navercorp.com>
2019-05-28 07:21:53 +0900

    Nextjs finished

--- first-next/pages/index.js
+++ first-next/pages/index.js
@@ -1,7 +1,53 @@
-const Index = () => (
-  <div>
-    <h1>Our Index Page!!!</h1>
-  </div>
-);
+import axios from "axios";
+import Link from "next/link";
+// class Index extends Component {
+//   constructor(props) {
+//     super(props);
+//   }
+
+//   static async getInitialProps() {
+//     const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+//     const { data } = res;
+//     return { posts: data };
+//   }
+
+//   render() {
+//     const { posts } = this.props;
+//     return (
+//       <div>
+//         <h1>Our Index Page!!!</h1>
+//         <ul>
+//           {posts.map(post => (
+//             <li key={post.id}>{post.title}</li>
+//           ))}
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
+
+const Index = ({ posts }) => {
+  console.log(posts);
+  return (
+    <div>
+      <h1>Our Index Page!!!</h1>
+      <ul>
+        {posts.map(post => (
+          <li key={post.id}>
+            <Link href={`/post?id=${post.id}`}>
+              <a>{post.title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+Index.getInitialProps = async () => {
+  const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+  const { data } = res;
+  return { posts: data };
+};
 
 export default Index;
